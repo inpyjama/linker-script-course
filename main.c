76012749c3e09ed0527899dafdd0dfc985f09466
nadgirabhishek <Abhishek.Nadgir@colorado.edu>
2025-07-13 23:02:25 +0000

    understanding linker list

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
  * Copyright © 2024 inpyjama.com. All Rights Reserved.
  * Author: Piyush Itankar <piyush@inpyjama.com>
  */
-extern int a;
-extern int bar(void);
-int foo (){
-    int b=a+bar();
-    return b;
+int initalized_global =2;
+int unitialisez_global[4];
+static int unitalized_global_static;
+static int initalized_global_static=3;
+
+void foo(){
+    int local_variable=4;
+    static int static_unitalized_local_variable;
+    static int static_initalized_local_variable=5;
+    return;
 }
